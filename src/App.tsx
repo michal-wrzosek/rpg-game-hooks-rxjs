87bc627270cb9eb6d0712f968b45269f2426007a
Michał Wrzosek <michal@wrzosek.pl>
2019-05-11 14:49:36 +0200

    Added GitHub Pages and fixed a hint text

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import styled from 'styled-components';
 
 import { Canvas } from "containers/Canvas/Canvas";
 
+const HintText = styled.div`
+  text-align: center;
+  padding: 10px;
+`;
 const Wrapper = styled.div`
   padding: 20px;
 `;
@@ -11,7 +15,7 @@ function App() {
   return (
     <Wrapper>
       <Canvas />
-      <div>Use Arrow Keys to move black dot</div>
+      <HintText>Use Arrow Keys to move that black dot</HintText>
     </Wrapper>
   );
 }
